0238f19039aa8184d25edf2ef3347a61e0f77f7e
Nezze <nezzeman@gmail.com>
2026-05-10 20:50:45 +0200

    få skiten att funka

--- src/main/java/com/nezze/freaknlazaz/FreaknLazaz.java
+++ src/main/java/com/nezze/freaknlazaz/FreaknLazaz.java
@@ -31,10 +31,10 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(freaknLazaz.MODID)
-public class freaknLazaz {
+@Mod(FreaknLazaz.MODID)
+public class FreaknLazaz {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "FreaknLazaz";
+    public static final String MODID = "freaknlazaz";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -64,7 +64,7 @@ public class freaknLazaz {
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public freaknLazaz(IEventBus modEventBus, ModContainer modContainer) {
+    public FreaknLazaz(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
